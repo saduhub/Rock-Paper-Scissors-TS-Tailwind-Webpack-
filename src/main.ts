67a07771b4b6fc0fcd5f5700e25f4ece0ddf0c0e
Jorge Duarte <sadujorge@gmail.com>
2023-10-15 21:14:39 -0700

    Comptuer choice shown after interval.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+let playerChoice: string;
+let computerChoice: string;
 // Rock, paper, scissors buttons
 const rock = document.querySelector('#rock') as HTMLDivElement;
 
@@ -8,6 +10,8 @@ rock.addEventListener( 'click', () => {
     scissors.classList.remove('bg-stone-400', 'border-stone-200');
     paper.classList.add('border-2', 'border-transparent', 'bg-stone-500');
     scissors.classList.add('border-2', 'border-transparent', 'bg-stone-500');
+    playerChoice = "rock"
+    console.log(playerChoice);
 })
 
 const paper = document.querySelector('#paper') as HTMLDivElement;
@@ -19,6 +23,8 @@ paper.addEventListener( 'click', () => {
     scissors.classList.remove('bg-stone-400', 'border-stone-200');
     rock.classList.add('border-2', 'border-transparent', 'bg-stone-500');
     scissors.classList.add('border-2', 'border-transparent', 'bg-stone-500');
+    playerChoice = "paper"
+    console.log(playerChoice);
 })
 
 const scissors = document.querySelector('#scissors') as HTMLDivElement;
@@ -30,12 +36,15 @@ scissors.addEventListener( 'click', () => {
     rock.classList.remove('bg-stone-400', 'border-stone-200');
     paper.classList.add('border-2', 'border-transparent', 'bg-stone-500');
     rock.classList.add('border-2', 'border-transparent', 'bg-stone-500');
+    playerChoice = "scissors"
+    console.log(playerChoice);
 })
 // Shoot button
 const shootButton = document.querySelector('#shoot') as HTMLButtonElement;
 const computerChoiceImage = document.querySelector('#computerChoiceImage') as HTMLImageElement;
 
-let imageSources: string[] = ["./assets/three.svg", "./assets/two.svg", "./assets/one.svg", "./assets/question.svg"];
+let imageSources: string[] = ["./assets/three.svg", "./assets/two.svg", "./assets/one.svg"];
+let imageSourcesOptions: string[] = ["./assets/paper.svg", "./assets/rock.svg", "./assets/scissor.svg"];
 let currentIndex: number = 0;
 // Each interval is assigned number. Undefined beforehand.
 let interval: number | undefined;
@@ -55,4 +64,10 @@ shootButton.addEventListener('click', () => {
             clearInterval(interval);
         }
     }, 1000);
+
+    const randomNumber: number = Math.floor(Math.random() * 3);
+
+    setTimeout(() => {
+        computerChoiceImage.src = imageSourcesOptions[randomNumber];
+    }, 4000)
 });
